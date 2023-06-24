8c56fa1161d25bc0388f18f98cddad01c5b3b1bb
Shivam <eshivam360@gmail.com>
2023-06-24 10:30:04 +0530

    Added authentication

---
+++ Frontend/src/Login.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+const Login=()=>{
+    const [userName,setUserName]=useState("");
+    const [password,setPassword]=useState("");
+
+    const navigate = useNavigate();
+
+    const setAuthToken = token => {
+        if (token) {
+            axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+        }
+        else
+            delete axios.defaults.headers.common["Authorization"];
+     }
+
+    const addToList = () =>{
+        console.log("button working!!")
+        axios.post("http://localhost:3001/login",{
+            userName,
+            password
+        }).then((res)=>{
+            const token=res.data.token
+            setAuthToken(token);
+        })
+        const path=`/`
+        navigate(path)
+    }
+    
+    const goToRegister=()=>{
+        const path=`/register`
+        navigate(path)
+    }
+
+    return (
+        <div className="details">
+            <div className="fields">
+                <h2>Login as student</h2>
+                <div className="form">
+                    <div className="inputBox">
+                        <input type="text" required value={userName} onChange={(e)=>{
+                            setUserName(e.target.value);
+                        }}/>
+                        <label>Username</label>
+                    </div>
+                    <div className="inputBox">
+                        <input type="password" required value={password} onChange={(e)=>{
+                            setPassword(e.target.value);
+                        }}/> 
+                        <label>Password</label>
+                    </div>
+                    <button className="btn" onClick={addToList}>Login</button>
+                    <div className="wannaSwitch">
+                        <p>Not Registered Yet? <button onClick={goToRegister}>Register</button></p>
+                    </div>
+                </div>
+            </div>     
+        </div>
+    );
+}
+
+export default Login;
--- Server/index.js
+++ Server/index.js
@@ -47,3 +47,73 @@ app.delete("/delete/:id",async (req, res) =>{
    await WorkModel.findByIdAndRemove(id).exec();
    res.send("deleted");
 });
+
+// Registration
+
+app.post("/register",async(req,res)=>{
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password,10)
+        const user = {userName: req.body.userName , password: hashedPassword}
+        const newUser = new studentModel({userName: user.userName, password: user.password})
+        await newUser.save()
+        res.status(201).send()
+    } catch{
+        res.status(500).send()
+    }    
+})
+
+// Authentication
+
+function authenticateUser(req, res, next) {
+    console.log(req.headers)
+    const authHeader = req.headers.authorization
+    console.log(authHeader)
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) {
+      console.log("Token is null")
+      return res.sendStatus(401)
+    }
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) {
+        console.log(err)
+        return res.sendStatus(403)
+      }
+      req.user = user
+      console.log(user)
+      if(user.isUser==false){
+          console.log("User is not a student")
+          return res.sendStatus(403)
+      }
+      next()
+    })
+  }
+
+// Login
+
+app.post("/login",async(req,res)=>{
+    
+    try{
+        console.log("Tried.")
+        const user=await studentModel.findOne({userName: req.body.userName})
+        console.log("User....")
+        if(!user){
+            console.log("No such user exists")
+            res.sendStatus(400);
+        }
+        const match=await bcrypt.compare(req.body.password,user.password)
+        console.log(match)
+        if(match){
+            // console.log("matched")
+            // console.log(user,process.env.ACCESS_TOKEN_SECRET)
+            const token=jwt.sign({id: user._id, isUser: true},process.env.ACCESS_TOKEN_SECRET)
+            res.json({user:user,token:token})
+        }
+        else{
+            console.log("Password incorrect")
+            res.send("Password incorrect")
+        }
+    } catch{
+        res.status(500).send()
+    }    
+})
